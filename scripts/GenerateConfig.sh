fe07539395bdc6e2127d00e034f7560adbecc691
we2oxy <we2oxy@gmail.com>
2021-10-31 21:38:44 +0800

    commit 2021-10-31_213844

--- scripts/GenerateConfig.sh
+++ scripts/GenerateConfig.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
 
-fun_lede_n1(){
-	echo "$funcname action"
+function fun_lede_n1(){
+	echo "$FUNCNAME action"
 	cd /build/lede-N1
 	make defconfig
 	./scripts/diffconfig.sh > /build/buildinfo/N1-openwrt.buildinfo
 	cat /build/buildinfo/N1-openwrt.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 }
 
-fun_lede_R2S(){
-	echo "$funcname action"
+function fun_lede_R2S(){
+	echo "$FUNCNAME action"
 	cd /build/lede-R2S
 	make defconfig
 	./scripts/diffconfig.sh > /build/buildinfo/R2S-lede.buildinfo
 	cat /build/buildinfo/R2S-lede.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 }
 
-fun_lede_x64(){
-	echo "$funcname action"
+function fun_lede_x64(){
+	echo "$FUNCNAME action"
 	cd /build/lede-x64
 	make defconfig
 	./scripts/diffconfig.sh > /build/buildinfo/x86-lede.buildinfo
 	cat /build/buildinfo/x86-lede.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 }
 
-fun_lede_1701(){
-	echo "$funcname action"
+function fun_lede_1701(){
+	echo "$FUNCNAME action"
 	cd /build/lede-1701
 	make defconfig
 	./scripts/diffconfig.sh > /build/buildinfo/x86-lede-1701.buildinfo
 	cat /build/buildinfo/x86-lede-1701.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 }
 
-fun_md5_ck(){
+function fun_md5_ck(){
 	ls -lh /build/buildinfo/*
 	md5sum /build/buildinfo/*.buildinfo
 }
--- scripts/GenerateConfig_init.sh
+++ scripts/GenerateConfig_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fun_addluciapp(){
-	echo "$funcname action"
+function fun_addluciapp(){
+	echo "$FUNCNAME action"
 	rm -rf tmp/
 	echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 	echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
@@ -14,41 +14,41 @@ fun_addluciapp(){
 	git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/lean/OpenClash
 	./scripts/feeds update -a
 	./scripts/feeds install -a
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 }
 
-fun_N1(){
-	echo "$funcname action"
+function fun_N1(){
+	echo "$FUNCNAME action"
 	git clone https://github.com/coolsnowwolf/lede.git /build/lede-N1
 	cd /build/lede-N1
 	fun_addluciapp
 	wget -O .config https://raw.githubusercontent.com/we2oxy/Build-Lede/master/buildinfo/N1-openwrt.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 	sleep 3
 }
 
-fun_R2S(){
-	echo "$funcname action"
+function fun_R2S(){
+	echo "$FUNCNAME action"
 	git clone https://github.com/coolsnowwolf/lede.git /build/lede-R2S
 	cd /build/lede-R2S
 	fun_addluciapp
 	wget -O .config https://raw.githubusercontent.com/we2oxy/Build-Lede/master/buildinfo/R2S-lede.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 	sleep 3
 }
 
-fun_x64(){
-	echo "$funcname action"
+function fun_x64(){
+	echo "$FUNCNAME action"
 	git clone https://github.com/coolsnowwolf/lede.git /build/lede-x64
 	cd /build/lede-x64
 	fun_addluciapp
 	wget -O .config https://raw.githubusercontent.com/we2oxy/Build-Lede/master/buildinfo/x86-lede.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 	sleep 3
 }
 
-fun_lede1701(){
-	echo "$funcname action"
+function fun_lede1701(){
+	echo "$FUNCNAME action"
 	git clone --branch lede-17.01 --single-branch  https://github.com/coolsnowwolf/openwrt.git /build/lede-1701
 	cd /build/lede-1701
 	rm -rf tmp/
@@ -63,7 +63,7 @@ fun_lede1701(){
 	./scripts/feeds update -a
 	./scripts/feeds install -a
 	wget -O .config https://raw.githubusercontent.com/we2oxy/Build-Lede/master/buildinfo/x86-lede-1701.buildinfo
-	echo "$funcname successful"
+	echo "$FUNCNAME successful"
 	sleep 3
 }
 
